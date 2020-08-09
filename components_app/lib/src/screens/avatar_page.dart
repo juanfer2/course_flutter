de0948d51a5fe4754d5c4d9100acdbdd61793e37
= <=>
2020-08-09 17:22:15 -0500

    Avatar

--- components_app/lib/src/screens/avatar_page.dart
+++ components_app/lib/src/screens/avatar_page.dart
@@ -6,8 +6,34 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Avatar')),
-      body: Text('Avatar'),
+      appBar: AppBar(
+        title: Text('Avatar'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://silencio.com.ar/wp-content/uploads/2019/11/Eruca-2148-Ceci-2000.jpg?x30159'),
+              radius: 23.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.pink,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://silencio.com.ar/wp-content/uploads/2019/11/Eruca-2148-Ceci-2000.jpg?x30159'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      ),
     );
   }
 }
