dabda52239b719aa0b60ca2dc98b8113a7905a89
= <=>
2020-08-09 17:07:11 -0500

    Alerts

--- components_app/lib/src/screens/alert_page.dart
+++ components_app/lib/src/screens/alert_page.dart
@@ -15,6 +15,60 @@ class AlertPage extends StatelessWidget {
           Navigator.pop(context);
         },
       ),
+      body: Center(
+        child: RaisedButton(
+          color: Colors.green,
+          elevation: 1.5,
+          child: Text(
+            'Show Alert',
+            style: TextStyle(color: Colors.white),
+          ),
+          shape: StadiumBorder(),
+          onPressed: () => _showAlert(context),
+        ),
+      ),
     );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Title'),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Este es un gran contenido genial'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                FlutterLogo(
+                  size: 100.0,
+                )
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text(
+                  'Cancel',
+                  style: TextStyle(color: Colors.red),
+                ),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              FlatButton(
+                child: Text(
+                  'OK',
+                  style: TextStyle(color: Colors.green),
+                ),
+                onPressed: () => Navigator.of(context).pop(),
+              )
+            ],
+          );
+        });
   }
 }
